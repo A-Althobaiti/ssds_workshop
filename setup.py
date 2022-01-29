497dd282b33a7b488697b701c358d0f80e024f65
A-Althobaiti <34758996+A-Althobaiti@users.noreply.github.com>
2022-01-29 21:33:32 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="easy_tools",
+      version="0.1",
+      description="Easy tools to facilitate faster pace in generating boilerplate code",
+      author="A.Althobaiti",
+      author_email="althobaiti@dot.com",
+      url="",
+      packages=["easy_tools"],
+)
