b34fa0836fd98fc3d0b5af29570e45626dec92ae
althobaiti <althobaiti@mozn.sa>
2022-02-18 20:44:19 +0300

    chore: add local package

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
+from distutils.core import setup
 import gensim.downloader as api
 
+
 api.load("glove-twitter-25") 
+
+
+setup(name="easy_tools",
+      version="0.1",
+      description="Easy tools to facilitate faster pace in generating boilerplate code",
+      author="A.Althobaiti",
+      author_email="althobaiti@dot.com",
+      url="",
+      packages=["easy_tools"],
+)
