7731b5863a4f9e01541ae1113984b98744cd3312
althobaiti <althobaiti@mozn.sa>
2022-02-18 20:37:02 +0300

    chore: update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from distutils.core import setup
+import gensim.downloader as api
 
-setup(name="easy_tools",
-      version="0.1",
-      description="Easy tools to facilitate faster pace in generating boilerplate code",
-      author="A.Althobaiti",
-      author_email="althobaiti@dot.com",
-      url="",
-      packages=["easy_tools"],
-)
+api.load("glove-twitter-25") 
